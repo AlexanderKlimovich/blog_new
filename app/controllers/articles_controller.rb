c4b89cfb3dd64300c5ce17d0b03490596f67683d
Alexandr <alexanderklimovich@gmail.com>
2017-01-05 15:51:33 +0200

    make function of save posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,22 @@
 class ArticlesController < ApplicationController
 
   def new
+    @article = Article.new	
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def create
-  	render plain: params[:article].inspect
-  end	
+   @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+    redirect_to @article
+  end
+
+# private
+#   def post_params
+#     params.require(:article).permit(:title, :text)
+#   end
 
 end
